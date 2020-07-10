5aa3c7675f1dfc02a1ff034b3391de9f571b2803
RenauxLouis <renauxlouis@gmail.com>
2020-07-10 01:05:38 -0400

    pull multiple files and clean

--- parse_pdf.py
+++ parse_pdf.py
@@ -1,8 +1,12 @@
-import tabula
-import sys
-import numpy as np
-import functools
 import argparse
+import functools
+import os
+import sys
+from collections import defaultdict
+
+import numpy as np
+import tabula
+from tqdm import tqdm
 
 
 def regex_per_word_wrapper(input_words, target_list):
@@ -46,36 +50,40 @@ def regex_per_word(match, list_r):
     return False
 
 
-file = "../Downloads/2019-Full-year-Results.pdf"
+def get_tables_from_pdf_file(fpath):
 
-tables = tabula.read_pdf(file, pages="all", multiple_tables=True)
-
-
-def get_tables_from_pdf_file():
-    map_target_table = {}
+    tables = tabula.read_pdf(fpath, pages="all", multiple_tables=True)
+    map_target_table = defaultdict(list)
     for i, table in enumerate(tables):
-        # print("#######", i)
         columns = table.columns
-        # first_row = list(table.iloc[0].dropna().values)
-        print(columns)
-        # print(table)
-        # if i == 8:
-        #     print(table)
-        #     table.to_csv("table_oui.csv")
-        #     sys.exit()
         for input_words in ["income statement", "balance sheet",
                             "financial position", "cash flow"]:
             results = regex_per_word_wrapper(input_words, columns)
-            # print(results)
             if results:
-                map_target_table[results[0]] = table
+                map_target_table[input_words].append(table)
     return map_target_table
 
 
+def main(input_folder):
+
+    map_target_table_per_year = {}
+    pdf_fnames = os.listdir(input_folder)
+    for fname in tqdm(pdf_fnames):
+        fpath = os.path.join(input_folder, fname)
+        year = fpath.split(".")[-2][-4:]
+        print(year)
+        map_target_table = get_tables_from_pdf_file(fpath)
+        map_target_table_per_year[year] = map_target_table
+    for year, value in map_target_table_per_year.items():
+        for key, subvalues in value.items():
+            for i, subvalue in enumerate(subvalues):
+                subvalue.to_csv(str(year) + "_" + key +
+                                "_" + str(i) + ".csv")
+
+
 def parse_args():
-    # Parse command line
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_folder", nargs="+")
+    parser.add_argument("--input_folder", type=str)
     parser.add_argument("--ticker_sec", type=str)
     args = parser.parse_args()
 
@@ -85,10 +93,10 @@ def parse_args():
 if __name__ == "__main__":
 
     args = parse_args()
-    if args.tickers:
-        tickers = args.tickers
+    if args.ticker_sec:
+        input_folder = download_10k_ticker(ticker_sec)
+        convert_htm_to_pdf(input_folder)
     else:
-        tickers_df = pd.read_csv(args.tickers_csv_fpath)
-        tickers = tickers_df["ticker"]
+        input_folder = args.input_folder
 
-    main(tickers)
+    main(input_folder)
