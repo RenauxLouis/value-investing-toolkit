115aaa6ff8b503df33489e3cef4c864ce4634200
RenauxLouis <renauxlouis@gmail.com>
2020-07-10 00:31:24 -0400

    script to parse tables from pdf

--- parse_pdf.py
+++ parse_pdf.py
@@ -2,6 +2,7 @@ import tabula
 import sys
 import numpy as np
 import functools
+import argparse
 
 
 def regex_per_word_wrapper(input_words, target_list):
@@ -49,13 +50,45 @@ file = "../Downloads/2019-Full-year-Results.pdf"
 
 tables = tabula.read_pdf(file, pages="all", multiple_tables=True)
 
-map_target_table = {}
-for i, table in enumerate(tables):
-    print("#######", i)
-    first_row = list(table.iloc[0].dropna().values)
-    print(table)
-    for input_words in ["income statement", "balance sheet"]:
-        result = regex_per_word_wrapper(input_words, first_row)
-        print(result)
 
-print(len(tables))
+def get_tables_from_pdf_file():
+    map_target_table = {}
+    for i, table in enumerate(tables):
+        # print("#######", i)
+        columns = table.columns
+        # first_row = list(table.iloc[0].dropna().values)
+        print(columns)
+        # print(table)
+        # if i == 8:
+        #     print(table)
+        #     table.to_csv("table_oui.csv")
+        #     sys.exit()
+        for input_words in ["income statement", "balance sheet",
+                            "financial position", "cash flow"]:
+            results = regex_per_word_wrapper(input_words, columns)
+            # print(results)
+            if results:
+                map_target_table[results[0]] = table
+    return map_target_table
+
+
+def parse_args():
+    # Parse command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_folder", nargs="+")
+    parser.add_argument("--ticker_sec", type=str)
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    if args.tickers:
+        tickers = args.tickers
+    else:
+        tickers_df = pd.read_csv(args.tickers_csv_fpath)
+        tickers = tickers_df["ticker"]
+
+    main(tickers)
