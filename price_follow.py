271f34665cc454797547996e227b77c2933631fd
RenauxLouis <renauxlouis@gmail.com>
2020-09-19 19:27:22 -0400

    allow adding tickers to db as csv

--- price_follow.py
+++ price_follow.py
@@ -1,11 +1,9 @@
 import argparse
 from datetime import date, timedelta
+from warnings import warn
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import yfinance as yf
-from matplotlib import patches
-from mplfinance import plot
 
 
 def print_prices(ticker, ticker_price_df):
@@ -16,13 +14,36 @@ def print_prices(ticker, ticker_price_df):
     print("#"*20, round(price, 1), "$ ", "#"*20, "\n")
 
 
-def main(csv_fpath, start_date, end_date):
+def add_tickers_to_db(tickers_to_add, df):
+
+    if tickers_to_add:
+        for ticker in tickers_to_add:
+            if ticker in df["ticker"].values:
+                msg = (f"Cannot add ticker '{ticker}' as it is already in the"
+                       " database")
+                warn(msg)
+                continue
+
+            strike_price = input("What is the strike price you want to set for"
+                                 f" ticker: {ticker}?: ")
+            df = df.append(
+                {"ticker": ticker, "strike_price": strike_price},
+                ignore_index=True)
+
+    return df
+
+
+def main(csv_fpath, start_date, end_date, tickers_to_add):
 
     df = pd.read_csv(csv_fpath)
-    tickers_to_follow = df["tickers"].values
+    tickers_to_follow = df["ticker"].values
     for ticker in tickers_to_follow:
         ticker_price_df = yf.download(ticker, start_date, end_date)
         print_prices(ticker, ticker_price_df)
+
+    df = add_tickers_to_db(tickers_to_add, df)
+
+    df.to_csv(csv_fpath)
 
 
 def parse_args():
@@ -33,6 +54,7 @@ def parse_args():
         "--csv_fpath", default="list_price_follow.csv", type=str)
     parser.add_argument("--start_date", default=date_1_year_ago, type=str)
     parser.add_argument("--end_date", default=date_today, type=str)
+    parser.add_argument("--tickers_to_add", nargs="+")
     args = parser.parse_args()
 
     return args
@@ -42,4 +64,5 @@ if __name__ == "__main__":
     args = parse_args()
     main(csv_fpath=args.csv_fpath,
          start_date=args.start_date,
-         end_date=args.end_date)
+         end_date=args.end_date,
+         tickers_to_add=args.tickers_to_add)
