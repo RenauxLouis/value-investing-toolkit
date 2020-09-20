960120c58ebd172fa1ce77245a9d7ba1734a7335
RenauxLouis <renauxlouis@gmail.com>
2020-09-20 00:47:31 -0400

    add email sending

--- price_follow.py
+++ price_follow.py
@@ -4,6 +4,44 @@ from warnings import warn
 
 import pandas as pd
 import yfinance as yf
+import smtplib
+import ssl
+
+
+def create_secure_connection_and_send_mail(ticker, most_recent_price,
+                                           strike_price):
+    port = 465  # For SSL
+    password = input("Type your password and press enter: ")
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port,
+                          context=context) as server:
+        server.login("strike.price.notification@gmail.com", password)
+        send_mail(ticker, most_recent_price, strike_price, server)
+
+
+def send_mail(ticker, most_recent_price, strike_price, server):
+    sender_email = "strike.price.notificatio@gmail.com"
+    receiver_email = "renauxlouis@gmail.com"
+    message = f"""\
+    Subject: STRIKE PRICE {ticker.upper()}
+
+    Today's closing price on '{ticker}' was ${int(round(most_recent_price, 0))} which is below the strike price you set at ${strike_price}"""
+    server.sendmail(sender_email, receiver_email, message)
+
+
+def compare_current_to_strike_prices(csv_fpath):
+    df = pd.read_csv(csv_fpath)
+    for ticker, strike_price in zip(df["ticker"], df["strike_price"]):
+        date_today = date.today()
+        ticker_price_df = yf.download(
+            ticker, date_today - timedelta(days=7), date_today)
+        most_recent_price = ticker_price_df.iloc[-1]["Close"]
+        print(most_recent_price)
+        if most_recent_price < strike_price:
+            create_secure_connection_and_send_mail(
+                ticker, most_recent_price, strike_price)
 
 
 def print_prices(ticker, ticker_price_df):
@@ -44,6 +82,8 @@ def main(csv_fpath, start_date, end_date, tickers_to_add):
     df = add_tickers_to_db(tickers_to_add, df)
 
     df.to_csv(csv_fpath)
+
+    compare_current_to_strike_prices(csv_fpath)
 
 
 def parse_args():
